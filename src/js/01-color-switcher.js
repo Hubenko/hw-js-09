dfb45c9d65c262fcab5e18d4e2d85224c6d5c05c
Dmytro Hubenko <d.a.hubenko@gmail.com>
2022-11-06 08:47:51 +0200

    готово 1-е

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+  buttonOn: document.querySelector('button[data-start]'),
+  buttonOff: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+let timerId = null;
+console.log(refs);
+console.log(refs.buttonOn);
 
+refs.buttonOn.addEventListener('click', startColor);
+refs.buttonOff.addEventListener('click', stopColor);
+
+function startColor() {
+  timerId = setInterval(onChangeColor, 1000);
+  refs.buttonOn.disabled = true;
+}
+
+function onChangeColor() {
+  const currentColor = getRandomHexColor();
+  refs.body.style.backgroundColor = currentColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function stopColor() {
+  clearInterval(timerId);
+  refs.buttonOn.disabled = false;
+}
