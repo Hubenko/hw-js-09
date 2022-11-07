4c4adf7349bf0ba706ae83eb55ed39813973f2a9
Dmytro Hubenko <d.a.hubenko@gmail.com>
2022-11-07 22:26:57 +0200

    Сделал часть 2-го

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,9 +6,10 @@ const refs = {
   buttonStart: document.querySelector('button[data-start]'),
 };
 let currentTime = null;
+
 refs.input.addEventListener('click', flatpickr);
 refs.buttonStart.disabled = true;
-// refs.buttonStart.addEventListener('click', qwe);
+refs.buttonStart.addEventListener('click', timer.start());
 
 const options = {
   enableTime: true,
@@ -16,22 +17,37 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0].getTime() <= options.defaultDate.getTime()) {
+    if (selectedDates[0] <= options.defaultDate) {
       window.alert('Please choose a date in the future');
     } else {
       refs.buttonStart.disabled = false;
-      currentTime = selectedDates[0].getTime();
+      currentTime = selectedDates[0];
     }
-    console.log(selectedDates[0].getTime());
   },
 };
 
 flatpickr('#datetime-picker', options);
 
-const timer = {
-  start() {
-    const startTime = currentTime - options.defaultDate;
-    console.log(startTime);
-  },
-};
-timer.start();
+// const timer = {
+//   start() {
+//     setInterval(() => {
+//       const startTime = currentTime - options.defaultDate;
+//       console.log(startTime);
+//     }, 1000);
+//   },
+// };
+// timer.start();
+
+// function convertMs(ms) {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   const days = Math.floor(ms / day);
+//   const hours = Math.floor((ms % day) / hour);
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
